a952ffa31c6ee373f1554dd4b6fbfe08e8233dd3
Sohaib Ali Syed <sohaibali01@gmail.com>
2022-05-17 00:48:26 +0500

    Update subset_sum.py

--- subset_sum.py
+++ subset_sum.py
@@ -1,5 +1,6 @@
 
 def two_sum_On2(arr, S):
+  # naive solution having quadratic complexity O(n^2)
   outputList=[]
   for item in arr:
       for item2 in arr:
@@ -8,17 +9,16 @@ def two_sum_On2(arr, S):
   return outputList
 
 def two_sum_On(arr, S):
+  # best solution having linear complexity O(n)
+  # uses a dictionary to store elements of arr as key, val pair
+  # we iterate over all items, and S minus item is present in te dictionary, insert it into output list
   outputList=[]
   hashTable = dict()
-
   for item in arr: 
-
     diffItem = S - item
     if diffItem in hashTable.keys():
       outputList.append([item, diffItem])
-
     hashTable[item] = item
-
   return outputList
   
   
