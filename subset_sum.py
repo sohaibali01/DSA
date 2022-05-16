a30b9bac09cff694b0c6ef9a4e95f5e1ab3026b8
Sohaib Ali Syed <sohaibali01@gmail.com>
2022-05-17 00:39:48 +0500

    Update subset_sum.py

--- subset_sum.py
+++ subset_sum.py
@@ -1,11 +1,26 @@
 
-def two_sum(arr, S):
+def two_sum_On2(arr, S):
   outputList=[]
   for item in arr:
       for item2 in arr:
           if item!=item2 and item+item2==S:
-              outputList.append(item, item2)
+              outputList.append([item, item2])
+  return outputList
+
+def two_sum_On(arr, S):
+  outputList=[]
+  hashTable = dict()
+
+  for item in arr: 
+
+    diffItem = S - item
+    if diffItem in hashTable.keys():
+      outputList.append([item, diffItem])
+
+    hashTable[item] = item
+
   return outputList
   
+  
 if __name__ == '__main__':
-    print(two_sum([3,5,2-4,8,11],7))
+    print(two_sum_On2([3,5,2,-4,8,11],7))
